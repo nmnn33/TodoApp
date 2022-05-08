767ff85628ee7fd03af74d87b44c9ae34a69305b
Mikael <2001546@laurea.fi>
2022-05-08 18:54:01 +0300

    Added task for each day

--- src/app/tuesday/tuesday.page.ts
+++ src/app/tuesday/tuesday.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { PopoverController } from '@ionic/angular';
+import { Router } from '@angular/router';
+import { AddPage } from '../add/add.page';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore'
 
 @Component({
   selector: 'app-tuesday',
@@ -7,9 +11,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TuesdayPage implements OnInit {
 
-  constructor() { }
+  constructor(private popoverCtrl: PopoverController, private router: Router,
+    private afFirestore: AngularFirestoreModule) { }
+
+    add() {}
 
   ngOnInit() {
   }
 
-}
+  async itemAdd(ev) {
+    const popover = await this.popoverCtrl.create({
+      component: AddPage,
+      event: ev,
+      cssClass: 'custom-popover'
+    })
+  
+    await popover.present();
+  }
+  
+  }
